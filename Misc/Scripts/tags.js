b76a25ee4116fa40817985b8eac03eeba2434d42
6Bd2 <ldz1943@163.com>
2026-01-24 14:21:46 +0800

    Update tags.js

--- Misc/Scripts/tags.js
+++ Misc/Scripts/tags.js
@@ -1,37 +1,33 @@
-module.exports = async function start(params) {
+module.exports = async (params) => {
     const { obsidian, quickAddApi = app.plugins.plugins.quickadd.api } = params || {};
-
-    const inputs = await quickAddApi.requestInputs([
-        {id: 'operation', type: 'dropdown', options:['add', 'remove', 'replace']},
-        {id: 'tag', type: 'text'},
-        {id: 'newTag', type: 'text'},
+    
+    const { operation, tag, newTag } = await quickAddApi.requestInputs([
+        { id: 'operation', type: 'dropdown', options: ['add', 'remove', 'replace'] },
+        { id: 'tag', type: 'text' },
+        { id: 'newTag', type: 'text' },
     ]);
 
-    const {operation, tag, newTag} = inputs;
-    const files = app.vault.getMarkdownFiles();
+    const operations = {
+        add: (tags) => !tags.includes(tag) && tags.push(tag),
+        remove: (tags) => {
+            const index = tags.indexOf(tag);
+            return index > -1 && tags.splice(index, 1);
+        },
+        replace: (tags) => {
+            const index = tags.indexOf(tag);
+            return index > -1 && (tags[index] = newTag);
+        }
+    };
+
     let modified = 0;
-    for (const file of files) {
+    
+    for (const file of app.vault.getMarkdownFiles()) {
         await app.fileManager.processFrontMatter(file, (fm) => {
             fm.tags = fm.tags || [];
-            
-            if (operation === "add" && !fm.tags.includes(tag)) {
-                fm.tags.push(tag);
-                modified++;
-            } else if (operation === "remove") {
-                const index = fm.tags.indexOf(tag);
-                if (index > -1) {
-                    fm.tags.splice(index, 1);
-                    modified++;
-                }
-            } else if (operation === "replace") {
-                const index = fm.tags.indexOf(tag);
-                if (index > -1) {
-                    fm.tags[index] = newTag;
-                    modified++;
-                }
-            }
+            if (operations[operation](fm.tags)) modified++;
         });
     }
+    
     new obsidian.Notice(`Modified ${modified} files`);
     return modified;
 }
