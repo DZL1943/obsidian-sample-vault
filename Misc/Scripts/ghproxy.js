536773fe9544a14c53b98b3b300017812301a88e
6Bd2 <ldz1943@163.com>
2026-02-09 23:52:16 +0800

    .

--- Misc/Scripts/ghproxy.js
+++ Misc/Scripts/ghproxy.js
@@ -1,19 +1,35 @@
 const { ipcRenderer } = require("electron");
 
-function delegateIpcRendererSend(config) {
+function delegateIpcRendererSend(config, rules, transform) {
     const send = ipcRenderer.send;
+    const defaultKey = rules[rules.length - 1]?.key;
+    
     ipcRenderer.send = function (...args) {
         const [type, , e] = args;
         if (type === "request-url" && e?.url) {
-            for (const prefix in config) {
-                if (e.url.startsWith(prefix)) {
-                    e.url = e.url.replace(prefix, config[prefix]);
-                    console.debug(e.url);
-                    e.headers = {
-                        ...(e.headers || {}),
-                        "content-type": "application/x-www-form-urlencoded",
-                        "Access-Control-Allow-Origin": "*"
-                    };
+            for (const r of rules) {
+                if (r.regex.test(e.url)) {
+                    const proxy = config[r.key] || config[defaultKey];
+                    if (proxy) {
+                        const originalUrl = new URL(e.url);
+                        const proxyUrl = new URL(proxy);
+                        switch (transform) {
+                            case "replace":
+                                e.url = proxyUrl.href.replace(/\/+$/, '') + originalUrl.href.substring(originalUrl.origin.length);
+                                break;
+                            case "prepend":
+                                e.url = proxyUrl.href.replace(/\/?$/, '/') + originalUrl.href;
+                                break;
+                            default:
+                                break;
+                        }
+                        console.debug(e.url);
+                        e.headers = { 
+                            ...(e.headers || {}), 
+                            "content-type": "application/x-www-form-urlencoded", 
+                            "Access-Control-Allow-Origin": "*" 
+                        };
+                    }
                     break;
                 }
             }
@@ -23,9 +39,20 @@ function delegateIpcRendererSend(config) {
 }
 
 async function start(params, settings) {
-    const config = JSON.parse(settings.config || 'null');
-    delegateIpcRendererSend(config);
-    console.log("GitHub代理已启用");
+    const rules = [
+        { regex: /^https:\/\/raw\.githubusercontent\.com\//, key: 'github-raw' },
+        { regex: /^https:\/\/api\.github\.com\//, key: 'github-api' },
+        { regex: /\/releases\/download\//, key: 'github-download' },
+        { regex: /^https:\/\/gist\.github\.com\//, key: 'github-gist' },
+        { regex: /^https:\/\/github\.com\//, key: 'github' }
+    ];
+    
+    delegateIpcRendererSend(
+        JSON.parse(settings.config || "{}"),
+        rules,
+        settings.transform
+    );
+    console.log("Proxy Enabled.");
 }
 
 module.exports = {
@@ -36,14 +63,15 @@ module.exports = {
         options: {
             config: {
                 type: "textarea",
-                defaultValue: `{
-  "https://raw.githubusercontent.com/": "https://ghfast.top/https://raw.githubusercontent.com/",
-  "https://github.com/": "https://ghfast.top/https://github.com/",
-  "https://api.github.com/": "https://ghfast.top/https://api.github.com/"
-}`,
-                description:
-                    "输入JSON格式的代理配置。键为原始URL前缀，值为替换后的URL前缀。",
+                defaultValue: `{"github": "https://ghfast.top/"}`,
+                description: `代理配置. 格式 {key: url}. 支持的key及优先级顺序: github-raw, github-api, github-download, github-gist, github; 若未找到则用最后一个(github)`,
             },
+            transform: {
+                type: "dropdown",
+                defaultValue: "prepend",
+                options: ["prepend", "replace"],
+                description: "URL转换模式. prepend: 添加代理前缀; replace: 替换URL origin"
+            }
         },
     },
 };
